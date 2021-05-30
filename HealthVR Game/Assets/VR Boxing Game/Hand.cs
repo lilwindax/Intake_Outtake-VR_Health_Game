bf8a7b1f0f05e7a6499103b247bbce8d1aad5290
lilwindax <72056829+lilwindax@users.noreply.github.com>
2021-05-31 09:02:20 +1200

    Adding to VR Boxing Game

---
+++ HealthVR Game/Assets/VR Boxing Game/Hand.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hand : MonoBehaviour
+{
+    public LayerMask layer;
+    private Vector3 previousPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+
+        if(Physics.Raycast(transform.position, transform.forward, out hit, 0.05, layer))
+        {
+            Destroy(hit.transform.gameObject);
+        }
+
+        previousPos = transform.position;
+    }
+}
---
+++ HealthVR Game/Assets/VR Boxing Game/Spawner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+
+    public GameObject[] BoxingBags;
+    public Transform[] points;
+    public float spawntime = (60/130)*2;
+    private float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer>spawntime)
+        {
+            GameObject BoxingBag = Instantiate(BoxingBags[Random.Range(0, 2)], points[Random.Range(0, 4)]);
+            //BoxingBag.transform.localPosition = Vector3.zero;
+            //BoxingBag.transform.Rotate(transform.forward, 90 * Random.Range(0, 4));
+            timer -= spawntime;
+
+        }
+
+        timer += Time.deltaTime;
+
+
+    }
+}
